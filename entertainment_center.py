016f70138f8fd15e61041ed169208cc8f0d55b2b
Gregorius Marco <marcogregorius@gmail.com>
2017-11-11 20:22:43 +0800

    Fresh commit of entertainment_center.py

---
+++ entertainment_center.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 11 15:40:36 2017
+
+@author: gregoriusmarco
+"""
+
+import media
+
+# Creating instances of Movie class
+Three_Idiots=media.Movie("Three Idiots","https://klling.files.wordpress.com/2013/03/3-idiots-poster.jpg","https://www.youtube.com/watch?v=K0eDlFX9GMc")
+Train_to_Busan=media.Movie("Train to Busan","https://i.pinimg.com/originals/bf/cc/85/bfcc855d0b4cf1634917ed0799feb1ee.jpg","https://www.youtube.com/watch?v=pyWuHv2-Abk")
+Shawshank_Redemption=media.Movie("Shawshank Redemption","https://fanart.tv/fanart/movies/278/movieposter/the-shawshank-redemption-5321a6ee8e7e9.jpg","https://www.youtube.com/watch?v=6hB3S9bIaco")
+Spirited_Away=media.Movie("Spirited Away","https://fontmeme.com/images/USA_full-spirited-away-poster.jpg","https://www.youtube.com/watch?v=ByXuk9QqQkk&t=8s")
+IT=media.Movie("IT","http://www.joblo.com/images_arrownews/it-teaser-poster.jpg","https://www.youtube.com/watch?v=hAUTdjf9rko")
+Wonder_Woman=media.Movie("Wonder Woman","https://i0.wp.com/batman-news.com/wp-content/uploads/2017/05/Wonder-Woman-poster-Patty-Jenkins.jpg?quality=85&strip=info&ssl=1&w=800","https://www.youtube.com/watch?v=1Q8fG0TtVAY")
+
+# Adding the movies into movies list
+movies=[Three_Idiots, Train_to_Busan, Shawshank_Redemption, Spirited_Away, IT, Wonder_Woman]
