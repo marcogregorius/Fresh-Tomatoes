70cef4d41cfdeaf36cee242b4e2f3ad99af1f571
Gregorius Marco <marcogregorius@gmail.com>
2017-11-12 00:44:35 +0800

    Inserted whitespace around = operator and removed extra line at the end of line.

--- media.py
+++ media.py
@@ -1,14 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 11 15:11:12 2017
-
-@author: gregoriusmarco
-"""
 # Defining class of Movie
 class Movie(object):
     def __init__(self, title, poster_image_url, trailer_youtube_url):
-        self.title=title
-        self.poster_image_url=poster_image_url
-        self.trailer_youtube_url=trailer_youtube_url
-
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
